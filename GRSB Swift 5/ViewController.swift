b5748b7886987795dace64a348e7d8d6f75d23f9
jle5718 <jle5718@rit.edu>
2020-06-24 17:36:32 -0400

    Initial Commit

---
+++ GRSB Swift 5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GRSB Swift 5
+//
+//  Created by Jared Ebenstein on 6/24/20.
+//  Copyright © 2020 Jared Ebenstein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
